cd4727a0a7424311f553e5bd40477c7b61b0b31d
renatoabreu14 <renato.abreu.info@gmail.com>
2021-11-17 01:36:40 -0300

    Project created

---
+++ src/main/java/com/tads4/locar/LocarApplication.java
@@ -0,0 +1,13 @@
+package com.tads4.locar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tads4/locar/LocarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads4.locar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
